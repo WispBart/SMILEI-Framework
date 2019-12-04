1ce69b0d191f3d7652bc60d309430def0a72fe28
Bart Heijltjes <bart@wispfire.com>
2019-12-04 19:18:38 +0100

    Added test objects for Vokaturi plugin

---
+++ Assets/Debug/VokaturiToText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SMILEI.Vokaturi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VokaturiToText : MonoBehaviour
+{
+
+    public VokaturiMixerAsset Mixer;
+    public Text Display;
+    
+    void OnEnable()
+    {
+        (Mixer.Implementation as VokaturiMixer)?.Register();
+    }
+
+    void OnDisable()
+    {
+        (Mixer.Implementation as VokaturiMixer)?.Unregister();
+
+    }
+
+    void Update()
+    {
+        Display.text = Mixer.Implementation.GetValue().Value.ToString();
+    }
+}
